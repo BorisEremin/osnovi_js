c910afabb7ae22722572c434003642983de825c2
Boris <freya4817@gmail.com>
2024-12-07 02:25:00 +0300

    HW_2_done

---
+++ HW_2/task_1.js
@@ -0,0 +1,12 @@
+let num1 = parseFloat(prompt('Введите значение 1:'));
+let num2 = parseFloat(prompt('Введите значение 2:'));
+
+if (num1 <= 1 && num2 >= 3) {
+    
+    console.log('Условия выполнены');
+
+} else {
+
+    console.log('Условия не выполнены');
+
+}
---
+++ HW_2/task_2.js
@@ -0,0 +1,12 @@
+// Описание задачи: Перепишите следующий код с использованием тернарного
+// оператора:
+
+// let test = true;
+// if (test === true) {
+// console.log('+++');
+// } else {
+// console.log('---');
+// }
+
+let test = true;
+console.log(test ? '+++' : '---');
